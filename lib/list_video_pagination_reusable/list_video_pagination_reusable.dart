ad85bdc2fd83a968045e2e3887340673fc59278a
Mosayeb Masoumi <masoumi.projects.co@gmail.com>
2022-11-26 12:09:15 +0330

    pagination list

---
+++ lib/list_video_pagination_reusable/list_video_pagination_reusable.dart
@@ -0,0 +1,272 @@
+
+import 'package:better_player_example/list_video_pagination_reusable/reusable_video_list_controller.dart';
+import 'package:better_player_example/list_video_pagination_reusable/reusable_video_list_widget.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import '../list_video_main_example/video_list_data.dart';
+
+class ListVideoPaginationReusablePage extends StatefulWidget {
+  const ListVideoPaginationReusablePage({Key? key}) : super(key: key);
+
+  @override
+  State<ListVideoPaginationReusablePage> createState() => _ListVideoPaginationReusablePageState();
+}
+
+class _ListVideoPaginationReusablePageState extends State<ListVideoPaginationReusablePage> {
+
+
+  // pagination
+  final _baseUrl = "https://jsonplaceholder.typicode.com/posts";
+  int _page = 2;
+  final int _limit = 10;
+  bool _isFirstLoadRunning = false;
+  bool _hasNextPage = true;
+  bool _isLoadMoreRunning = false;
+
+  List<HomeModel> _totalPosts = [];
+
+  void _loadMore() async {
+    if (_hasNextPage == true &&
+        _isFirstLoadRunning == false &&
+        _isLoadMoreRunning == false &&
+        _scrollController.position.extentAfter <
+            300 // this line of clause is for appear progressbar after reach the end of list
+    ) {
+      setState(() {
+        _isLoadMoreRunning = true;
+      });
+      await Future.delayed(Duration(seconds: 2));
+      _page += 1;
+      // call data
+      try {
+        List<HomeModel> nextPageList = [];
+        if (_totalPosts.length < 50) {
+          // total amount 50
+          nextPageList = getList();
+        }
+
+        if (nextPageList.isNotEmpty) {
+          setState(() {
+            _totalPosts.addAll(nextPageList);
+          });
+        } else {
+          setState(() {
+            _hasNextPage = false;
+          });
+        }
+      } catch (err) {
+        if1(kDebugMode) {
+          print("Something went wrong");
+        }
+      }
+
+      setState(() {
+        _isLoadMoreRunning = false;
+      });
+    }
+  }
+
+  void _firstLoad() async {
+    setState(() {
+      _isFirstLoadRunning = true;
+    });
+
+    await Future.delayed(Duration(seconds: 2));
+    // call data
+    try {
+      setState(() {
+        _totalPosts = getList();
+      });
+    } catch (err) {
+      if (kDebugMode) {
+        print("Something went wrong");
+      }
+    }
+    setState(() {
+      _isFirstLoadRunning = false;
+    });
+  }
+
+  late ScrollController _scrollController;
+
+  ReusableVideoListController videoListController = ReusableVideoListController();
+  var value = 0;
+  final ScrollController _reusableScrollController = ScrollController();
+  int lastMilli = DateTime.now().millisecondsSinceEpoch;
+  bool _canBuildVideo = true;
+
+
+  @override
+  void dispose() {
+    videoListController.dispose();
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _firstLoad();
+    _scrollController = ScrollController()..addListener(_loadMore);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return  Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: buildCustomAppbar(context, size),
+        // body: SingleChildScrollView(
+        body: Column(
+          children: [
+            buildMainList(size),
+          ],
+        ),
+
+    );
+  }
+
+  PreferredSize buildCustomAppbar(BuildContext context, Size size) {
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(70.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 70,
+      ),
+    );
+  }
+
+
+
+
+  Widget buildMainList(Size size) {
+    return Expanded(
+      child: _isFirstLoadRunning
+          ? const Center(
+        child: CircularProgressIndicator(),
+      )
+          : Column(children: [
+        Expanded(
+          child: NotificationListener<ScrollNotification>(
+            onNotification: (notification) {
+              final now = DateTime.now();
+              final timeDiff = now.millisecondsSinceEpoch - lastMilli;
+              if (notification is ScrollUpdateNotification) {
+                final pixelsPerMilli =
+                    notification.scrollDelta! / timeDiff;
+                if (pixelsPerMilli.abs() > 1) {
+                  _canBuildVideo = false;
+                } else {
+                  _canBuildVideo = true;
+                }
+                lastMilli = DateTime.now().millisecondsSinceEpoch;
+              }
+
+              if (notification is ScrollEndNotification) {
+                _canBuildVideo = true;
+                lastMilli = DateTime.now().millisecondsSinceEpoch;
+              }
+
+              return true;
+            },
+            child: ListView.builder(
+              itemCount: _totalPosts.length,
+              shrinkWrap: true,
+              controller: _scrollController,
+              itemBuilder: (context, index) {
+                VideoListData videoListData = VideoListData(
+                    _totalPosts[index].title,
+                    _totalPosts[index].videoUrl);
+                return ReusableVideoListWidget(
+                    videoListData: videoListData,
+                    videoListController: videoListController,
+                    canBuildVideo: _checkCanBuildVideo,
+                    index: index);
+              },
+            ),
+          ),
+        ),
+
+        if(_isLoadMoreRunning == true)
+          const Padding(
+            padding: EdgeInsets.only(top: 10 ,bottom: 40),
+            child: Center(child: CircularProgressIndicator(),),
+          ),
+
+      ]),
+    );
+
+  }
+
+  List<HomeModel> getList() {
+    List<HomeModel> list = [];
+    list.add(HomeModel(
+        "title1",
+        "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/720/Big_Buck_Bunny_720_10s_1MB.mp4",
+        "Video"));
+    list.add(HomeModel(
+        "title2",
+        "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/720/Big_Buck_Bunny_720_10s_2MB.mp4",
+        "Video"));
+    list.add(HomeModel(
+        "title3",
+        "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/720/Big_Buck_Bunny_720_10s_5MB.mp4",
+        "Video"));
+    list.add(HomeModel(
+        "title4",
+        "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/720/Big_Buck_Bunny_720_10s_1MB.mp4",
+        "Video"));
+    list.add(HomeModel(
+        "title5",
+        "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/720/Big_Buck_Bunny_720_10s_2MB.mp4",
+        "Video"));
+    list.add(HomeModel(
+        "title6",
+        "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/720/Big_Buck_Bunny_720_10s_5MB.mp4",
+        "Video"));
+    list.add(HomeModel(
+        "title7",
+        "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/720/Big_Buck_Bunny_720_10s_1MB.mp4",
+        "Video"));
+    list.add(HomeModel(
+        "title8",
+        "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/720/Big_Buck_Bunny_720_10s_2MB.mp4",
+        "Video"));
+    list.add(HomeModel(
+        "title9",
+        "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/720/Big_Buck_Bunny_720_10s_5MB.mp4",
+        "Video"));
+    list.add(HomeModel(
+        "title10",
+        "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/720/Big_Buck_Bunny_720_10s_1MB.mp4",
+        "Video"));
+
+    return list;
+  }
+
+  bool _checkCanBuildVideo() {
+    return _canBuildVideo;
+  }
+
+
+
+}
+
+
+
+class HomeModel {
+  String title;
+  String videoUrl;
+  String type;
+
+  HomeModel(this.title, this.videoUrl, this.type);
+}
+
+class StoryModel {
+  String title;
+
+  StoryModel(this.title);
+}
+
---
+++ lib/splash.dart
@@ -0,0 +1,113 @@
+import 'package:better_player_example/list_video_main_example/video_list_page.dart';
+import 'package:better_player_example/list_video_pagination_reusable/list_video_pagination_reusable.dart';
+import 'package:better_player_example/list_video_reusable/reusable_video_list_page.dart';
+import 'package:better_player_example/single_video_example/better_player_page.dart';
+import 'package:better_player_example/slider/slider_page.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import 'list_video_example/list_video_page.dart';
+
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () async {
+                  // createFolders(type: "single");
+                  if (await Permission.storage.request().isGranted) {
+                    createFolders(type: "single");
+                  }
+                },
+                child: Text("single video player")),
+            ElevatedButton(
+                onPressed: () async {
+                  if (await Permission.storage.request().isGranted) {
+                    createFolders(type: "list");
+                  }
+                },
+                child: Text("list image video player")),
+            ElevatedButton(
+                onPressed: () async {
+                  if (await Permission.storage.request().isGranted) {
+                    createFolders(type: "slider");
+                  }
+                },
+                child: Text("slider image video player")),
+
+
+            ElevatedButton(
+                onPressed: () async {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => VideoListPage()),
+                  );
+                },
+                child: Text("video list main example page")),
+
+            ElevatedButton(
+                onPressed: () async {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ReusableVideoListPage()),
+                  );
+                },
+                child: Text("reusable video list page")),
+
+
+            ElevatedButton(
+                onPressed: () async {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ListVideoPaginationReusablePage()),
+                  );
+                },
+                child: Text("list video pagination reusable")),
+
+
+
+
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> createFolders({required String type}) async {
+    // await CreateFolder().betterExampleFolderExistence();
+    // await CreateFolder().imageFolderExistence();
+    // await CreateFolder().videoFolderExistence();
+
+    type == "single"
+        ? Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => VideoPlayerPage()),
+          )
+        : type == "slider"
+            ? Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SliderPage()),
+              )
+            : Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ListPlayerPage()),
+              );
+  }
+}
